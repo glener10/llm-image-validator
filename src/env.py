b325385b49d9e1073be92dce22407281a9c53d22
glener10 <glenerpizzolato@gmail.com>
2025-09-18 16:19:27 -0300

    feat: separate API key retrieval functions for Gemini and OpenAI

--- src/env.py
+++ src/env.py
@@ -1,10 +1,18 @@
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-def get_api_key():
-    load_dotenv()
-    api_key = os.getenv("API_KEY")
+
+def get_gemini_api_key():
+    api_key = os.getenv("API_KEY_GEMINI")
     if not api_key:
-        raise Exception("❌ missing API_KEY in .env")
+        raise Exception("❌ missing API_KEY_GEMINI in .env")
+    return api_key
+
+
+def get_openai_api_key():
+    api_key = os.getenv("API_KEY_OPENAI")
+    if not api_key:
+        raise Exception("❌ missing API_KEY_OPENAI in .env")
     return api_key
